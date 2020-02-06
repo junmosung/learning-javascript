96c079ba95e914871f8f255a82c4add5e5109cad
victorsung <victorsung.js@gmail.com>
2020-02-06 11:45:35 +0900

    add immutable.js

--- immutable.js
+++ immutable.js
@@ -1,1 +1,19 @@
-var 
+var user = {
+  name : "victor",
+  age : 100
+}
+
+var changName = function(user, newName){
+  var newUser = user;
+  user.name = newName;
+  return newUser;
+}
+
+var user2 = changName(user, "invincible");
+
+if(user !== user2){
+  console.log("유저가 변경되었습니다.");
+}
+
+console.log(user.name, user2.name);
+console.log(user === user2);
